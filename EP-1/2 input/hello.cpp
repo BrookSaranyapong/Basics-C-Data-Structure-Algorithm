699dc38967694891b2f3b87a325d206246ab0167
Brooksaranyapong <brook.saranyapong@gmail.com>
2024-02-11 01:32:00 +0700

    update Test input

---
+++ EP-1/2 input/hello.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    int age; 
+    cout << "กรุณาพิมพ์อายุเข้ามา: ";
+    cin >> age;
+    cout << "คุณอายุเท่ากับ " << age;
+    return 0;
+}
