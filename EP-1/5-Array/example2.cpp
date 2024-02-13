cba90d38d4350bcc9e3162ababc2c14e873b36ff
Brooksaranyapong <brook.saranyapong@gmail.com>
2024-02-13 23:00:19 +0700

    Array + String

---
+++ EP-1/5-Array/example2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string name;
+    cout << "คุณชื่ออะไร ? : ";
+    cin >> name;
+    cout << "คุณชื่อ = " << name << endl;
+    char firstCharacter = name[0];
+    cout << "ตัวอักษรตัวแรกของคุณคือ: " << firstCharacter << endl;
+
+    return 0;
+}
